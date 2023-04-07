6d0f621090783b2b5ec67c32e1c219db470a1105
zydnrbrn <zidanreborn@gmail.com>
2023-04-07 16:35:07 +0700

    disable eslint at production

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
   },
   assetPrefix: './',
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
